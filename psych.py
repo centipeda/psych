2d72bb798029c0c770fff4608f2954115a2dd4a1
j-crit <joshua.c.guam@gmail.com>
2016-02-18 01:46:55 +1000

    Oops. Now it's working right.

--- psych.py
+++ psych.py
@@ -1,8 +1,10 @@
+#!/bin/usr/env python
+
 """
 A simple 'personality test'.
 """
 
-from time import sleep
+import time
 
 def main():
     notes = open('psychnotes.txt','r+')
@@ -23,14 +25,15 @@ def main():
     print "What adjective do you think would describe you the best?"
     asknote(notes)
     print "What do you think is your best trait? Please be specific."
-    asknote(note)
+    asknote(notes)
     print "In school, what subject do you get the best grades in?"
-    asknote(note)
+    asknote(notes)
     print "What subject do you get the worst grades in?"
-    asknote(note)
+    asknote(notes)
     print "What is your favorite genre of music?"
-    asknote(note)
+    asknote(notes)
     print "Finally, in one sentence, how do you think other people see you?"
+    asknote(notes)
     notes.close()
 
     raw_input("Questionnaire finished. Press enter to display your results.")
@@ -49,12 +52,16 @@ def main():
     time.sleep(2.0)
     print "Finalizing..."
     time.sleep(5.0)
-    input("Finished! Press enter to view your results.")
+    raw_input("Finished! Press enter to view your results.\n\n")
 
-    print "You have a need for other people to like and admire you, and yet you tend to be critical of yourself. While you have some personality weaknesses, you are generally able to compensate for them. You have considerable unused capacity that you have not turned to your advantage. Disciplined and self-controlled on the outside, you tend to be worrisome and insecure on the inside. At times you have serious doubts as to whether you have made the right decision or done the right thing. You prefer a certain amount of change and variety and become dissatisfied when hemmed in by restrictions and limitations. You also pride yourself as an independent thinker and do not accept others' statements without satisfactory proof. But you have found it unwise to be too frank in revealing yourself to others. At times you are extroverted, affable, and sociable, while at other times you are introverted, wary, and reserved. Some of your aspirations tend to be rather unrealistic." 
+    print "\tYou have a need for other people to like and admire you, and yet you tend to be critical of yourself. While you have some personality weaknesses, you are generally able to compensate for them. You have considerable unused capacity that you have not turned to your advantage. Disciplined and self-controlled on the outside, you tend to be worrisome and insecure on the inside. At times you have serious doubts as to whether you have made the right decision or done the right thing. You prefer a certain amount of change and variety and become dissatisfied when hemmed in by restrictions and limitations. You also pride yourself as an independent thinker and do not accept others' statements without satisfactory proof. But you have found it unwise to be too frank in revealing yourself to others. At times you are extroverted, affable, and sociable, while at other times you are introverted, wary, and reserved. Some of your aspirations tend to be rather unrealistic." 
+    print "\n\n"
 
-   print "Please do not share your results with others. Knowledge of the test may compromise others' results!" 
+    print "Please do not share your results with others. Knowledge of the test may compromise others' results!" 
 
 def asknote(note):
     n = raw_input("> ")
     note.write(n + "\t")
+
+if __name__ == '__main__':
+    main()
